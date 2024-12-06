9aa5c8ad1cf8e8b2e67068c19536fc2107151991
oniontherock <orionmartin049@gmail.com>
2024-12-06 12:07:30 -0500

    Got rid of some type conversion errors

--- Engine Modules/Audio/SoundPlayer.cpp
+++ Engine Modules/Audio/SoundPlayer.cpp
@@ -7,7 +7,7 @@ void SoundPlayer::soundsUpdate() {
 		sounds.pop_front();
 	}
 
-	for (int16_t i = sounds.size() - 1; i >= 0; i--) {
+	for (int16_t i = int16_t(sounds.size() - 1); i >= 0; i--) {
 		if (sounds[i].getStatus() != sf::Sound::Playing) {
 			sounds.erase(sounds.begin() + i);
 		}
--- Engine Modules/ECS/Entities/Entity.cpp
+++ Engine Modules/ECS/Entities/Entity.cpp
@@ -113,7 +113,7 @@ void Entity::componentsAllTerminate() {
 }
 
 void Entity::entityEventTerminateAllOfType(EntityEvents::EventTypeID eventId) {
-	for (int32_t i = eventsVector[eventId].size() - 1; i >= 0; i--) {
+	for (int32_t i = int32_t(eventsVector[eventId].size() - 1); i >= 0; i--) {
 		entityEventTerminate(eventId, i);
 	}
 }
