e0b1bd417f7bfe4ec74f39a66162737b77411556
oniontherock <orionmartin049@gmail.com>
2024-12-16 20:10:36 -0500

    added inline to all functions in Math

--- Engine Modules/Auxiliary/Math.hpp
+++ Engine Modules/Auxiliary/Math.hpp
@@ -9,14 +9,14 @@ public:
 	static constexpr T PI = T(3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679);
 	static constexpr T TAU = T(6.283185307179586476925286766559005768394338798750216419498891846156328125724179972560696506842341359);
 
-	static bool approxEquals(T a, T b, float epsilon) {
+	inline static bool approxEquals(T a, T b, float epsilon) {
 		return std::abs(a - b) < epsilon;
 	}
-	static int sign(T v) {
+	inline static int sign(T v) {
 		return (T(0) < v) - (v < T(0));
 	}
 
-	static T slerp(T a, T b, T v) {
+	inline static T slerp(T a, T b, T v) {
 		T diff = fmod(b - a, TAU);
 		T dist = fmod(2.0f * diff, TAU) - diff;
 		return a + dist * v;
