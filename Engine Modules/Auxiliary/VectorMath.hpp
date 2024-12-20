b4c0df9d142e0564b1f943518381214a475934e3
oniontherock <orionmartin049@gmail.com>
2024-12-20 15:35:44 -0500

    added some functions for checking line intersection to Vector2Math

--- Engine Modules/Auxiliary/VectorMath.hpp
+++ Engine Modules/Auxiliary/VectorMath.hpp
@@ -128,6 +128,62 @@ public:
 	static Vector2 abs(Vector2 vector) {
 		return abs(vector.x, vector.y);
 	}
+
+
+	// Given three collinear points p, q, r, the function checks if 
+	// point q lies on line segment 'pr' 
+	static bool onSegment(Vector2 p, Vector2 q, Vector2 r)
+	{
+		if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
+			q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y))
+			return true;
+
+		return false;
+	}
+	// To find orientation of ordered triplet (p, q, r). 
+	// The function returns following values 
+	// 0 --> p, q and r are collinear 
+	// 1 --> Clockwise 
+	// 2 --> Counterclockwise 
+	static uint16_t orientation(Vector2 p, Vector2 q, Vector2 r) {
+		// for details of below formula. 
+		int val = (q.y - p.y) * (r.x - q.x) -
+			(q.x - p.x) * (r.y - q.y);
+
+		if (val == 0) return 0;  // collinear 
+
+		return (val > 0) ? 1 : 2; // clock or counterclock wise 
+	}
+	// The main function that returns true if line segment 'p1q1' 
+	// and 'p2q2' intersect. 
+	static bool linesIntersect(Vector2 p1, Vector2 q1, Vector2 p2, Vector2 q2)
+	{
+		// Find the four orientations needed for general and 
+		// special cases 
+		int o1 = orientation(p1, q1, p2);
+		int o2 = orientation(p1, q1, q2);
+		int o3 = orientation(p2, q2, p1);
+		int o4 = orientation(p2, q2, q1);
+
+		// General case 
+		if (o1 != o2 && o3 != o4)
+			return true;
+
+		// Special Cases 
+		// p1, q1 and p2 are collinear and p2 lies on segment p1q1 
+		if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+		// p1, q1 and q2 are collinear and q2 lies on segment p1q1 
+		if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+		// p2, q2 and p1 are collinear and p1 lies on segment p2q2 
+		if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+		// p2, q2 and q1 are collinear and q1 lies on segment p2q2 
+		if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+		return false; // Doesn't fall in any of the above cases 
+	}
 };
 
 using Vector2fMath = Vector2Math<float, float>;
